b47882106a229662f41b23579790394201d857b8
Vinay Ghate <47355614+vinay-ghate@users.noreply.github.com>
2023-03-24 20:01:07 +0530

    Added Day 1 Code

---
+++ Day 1/largest_and_smallest.py
@@ -0,0 +1,13 @@
+# 🎉 This is the solution for Day 1 of the IODump Chaitra Challenge!
+
+# 💻 The task was to find the largest and smallest numbers out of the given 5,
+
+# and with hard work and dedication, you have successfully achieved it! 🙌
+# Your problem-solving skills and determination are impressive, and you should 
+# feel proud of your accomplishment. Let's move on to the next challenge with 
+# confidence and enthusiasm! 💪
+
+a = []
+for i in range(5):
+    a.append(int(input()))
+print("Largest Number : %d"%(max(a)),"\nSmallest Number : %d"%(min(a)))
